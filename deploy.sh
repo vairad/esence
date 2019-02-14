671d4d3d4997e97cf41ffaaacac571397f7ea3b0
Olie <vais.radek@gmail.com>
2019-02-14 13:04:42 +0100

    add main.css to deploy

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,6 @@ cp index.html /var/www/esence/
 cp pribehy.html /var/www/esence/
 cp pravidla.html /var/www/esence/
 cp about.html /var/www/esence/
+cp main.css /var/www/esence/
 cp -r img /var/www/esence
 
